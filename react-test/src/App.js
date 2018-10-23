c0d470cde804e56cbd566902e42a709526369dae
tochukz <truetochukz@gmail.com>
2018-10-23 06:07:39 +0200

    clock time

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import Users from './users';
+import MemberList from './components/member-list';
+import Clock from './components/clock';
+
 
 class App extends Component {
   render() {
-    return (
+    const target = document.getElementById('clock');
+    return (     
       <div className="App">
+        <Clock onClose={() => ReactDOM.unmountComponentAtNode(target)}/>
+        <hr />
         <Users title="Users List" />
+        <hr />
+        <MemberList count={5} />        
       </div>
     );
   }

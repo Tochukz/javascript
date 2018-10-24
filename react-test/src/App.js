7687d6433aa6defe9d5efec8891515a4907b799b
tochukz <truetochukz@gmail.com>
2018-10-24 06:01:35 +0200

    'hidden messages'

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -5,18 +5,31 @@ import './App.css';
 import Users from './users';
 import MemberList from './components/member-list';
 import Clock from './components/clock';
+import HiddenMessages from './components/hidden-messages';
+import Display from './components/display';
+import CountryList from './components/country-list';
 
 
 class App extends Component {
   render() {
     const target = document.getElementById('clock');
+    const age = 23;
     return (     
       <div className="App">
         <Clock onClose={() => ReactDOM.unmountComponentAtNode(target)}/>
         <hr />
+        <HiddenMessages />
+        <hr />
+        <Display truthy={(age > 21)}>
+          <p>This display component can only contain one child.<br />
+            More than one child will make react to through  an error.</p>          
+        </Display>
+        <hr />
         <Users title="Users List" />
         <hr />
-        <MemberList count={5} />        
+        <MemberList count={5} />    
+        <hr />
+        <CountryList />    
       </div>
     );
   }
